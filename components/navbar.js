09aa2a1d1daf3b1499c3043054bed224e1263be4
ビクター <84461857+vloax@users.noreply.github.com>
2023-02-16 07:01:30 -0400

    app install and component creation

---
+++ components/navbar.js
@@ -0,0 +1,48 @@
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Form from 'react-bootstrap/Form';
+
+function MainNavbar() {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">My Academy Tasks</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+          <NavDropdown title="Minhas Listas" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#features">Professores</Nav.Link>
+            <Nav.Link href="#pricing">Turmas</Nav.Link>
+            <Nav.Link href="#pricing">Disciplinas</Nav.Link>
+            <Nav.Link href="#pricing">Cursos</Nav.Link>
+            <Nav.Link href="#pricing">Usuários</Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default MainNavbar;
---
+++ pages/homepage.js
@@ -0,0 +1,13 @@
+import Navbar from "components/navbar";
+import Card from "react-bootstrap/Card";
+import styles from "../styles/Home.module.css";
+
+function HomePageFull() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default HomePageFull;
